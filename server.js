d27a46a658bbb160f4ffa4978a13343b10bf211d
lupinus <bzirs@outlook.com>
2025-09-01 21:57:09 +0800

    连接mongodb

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const fsPromises = require('node:fs/promises');
 
 const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+const mongoose = require('mongoose');
 
-dotenv.config({
-  path: ['.env', 'config.env'],
-});
+// 识别.env中${}内容
+dotenvExpand.expand(
+  dotenv.config({
+    path: ['.env', 'config.env'],
+  }),
+);
 
 const { setTours } = require('./controller/tours');
 
@@ -14,6 +19,14 @@ const app = require('./app');
 
 app.listen(port, async (_) => {
   try {
+    // 连接数据库
+    const mongo = await mongoose.connect(process.env.DATABASE_ADDRESS, {
+      // 连接数据库设置从默认admin数据库中认证
+      authSource: 'admin',
+    });
+
+    if (mongo) console.log('mongoDB 连接成功 ✌️');
+
     // TODO read file simple tours list
     setTours(
       JSON.parse(await fsPromises.readFile(`${__dirname}/dev-data/data/tours-simple.json`, { encoding: 'utf8' })),
@@ -21,6 +34,6 @@ app.listen(port, async (_) => {
 
     console.log(`app is running on: http://localhost:${port}`);
   } catch (e) {
-    console.log(e);
+    console.log(e.message);
   }
 });
