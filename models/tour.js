116ab6324d87e7f2b1d18424c66df18d1c0ce5c8
lupinus <bzirs@outlook.com>
2025-09-08 22:42:55 +0800

    :new: feat: 新增mongoose 中间件功能

--- models/tour.js
+++ models/tour.js
@@ -1,59 +1,120 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // 创建集合
-const TourShema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Tour name is required'],
-    // 唯一性校验
-    unique: true,
-    trim: true,
-  },
-  duration: {
-    type: Number,
-    required: [true, 'Tour duration is required'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Tour max group size is required'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'Tour difficulty is required'],
-  },
+const TourShema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Tour name is required'],
+      // 唯一性校验
+      unique: true,
+      trim: true,
+    },
+    secretField: {
+      type: String,
+      select: false, // 默认隐藏
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'Tour duration is required'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Tour max group size is required'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Tour difficulty is required'],
+    },
 
-  ratingAverage: {
-    type: Number,
-    default: 3.9,
+    ratingAverage: {
+      type: Number,
+      default: 3.9,
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
+    },
+    rating: { type: Number, default: 4.1 },
+    price: { type: Number, required: [true, 'Tour price is required'] },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Tour summary is required'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Tour cover image is required'],
+    },
+    images: {
+      type: [String],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
-  ratingQuantity: {
-    type: Number,
-    default: 0,
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
   },
-  rating: { type: Number, default: 4.1 },
-  price: { type: Number, required: [true, 'Tour price is required'] },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'Tour summary is required'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'Tour cover image is required'],
-  },
-  images: {
-    type: [String],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
+);
+
+// 文档 中间件 pre 保存 创建之前执行 .save() .create()  update 不会执行
+TourShema.pre('save', function (next) {
+  console.log('first  pre save middleware');
+  this.slug = slugify(this.name, { lower: true });
+
+  next();
+});
+
+// 某个事件执行后执行 post
+// TourShema.post('save', (doc, next) => {
+//   console.log('this is post save middleware', this, doc);
+//   next();
+// });
+//
+// // 执行顺序 save 的pre  > save的post > update的pre > update的post
+// TourShema.pre('save', (next) => {
+//   console.log('will save document...');
+//   next();
+// });
+
+TourShema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  // 添加属性
+  this.startTime = Date.now();
+  next();
+});
+TourShema.post(['find', 'findOne'], function (docs, next) {
+  console.info(`time is ${Date.now() - this.startTime} ms`);
+  next();
+});
+
+TourShema.post('create', (doc, next) => {
+  console.log('this is post create middleware', doc);
+  next();
+});
+
+//  虚拟属性 通过数据库中真实字段 计算出的属性， 通过getter获取 不能通过虚拟属性进行查询数据库 find xxx
+TourShema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', TourShema);
