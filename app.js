b44ba427dd6b19193d6473670909ff54420c18d6
lupinus <bzirs@outlook.com>
2025-09-11 21:14:33 +0800

    :new: feat: 添加其他未识别的路由进行处理中间件，添加错误处理中间件

--- app.js
+++ app.js
@@ -31,4 +31,29 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
+// 所有路由都匹配不到时 对其他未识别的路由进行处理 . 定义 404 错误传递给最后的错误处理中间件
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 404,
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const e = new Error(`Can't find ${req.originalUrl} on this server!`);
+  e.statusCode = 404;
+  e.status = '404';
+
+  next(e);
+});
+
+// 错误处理
+app.use((err, req, res, next) => {
+  err.statusCode ||= 500; // 逻辑或赋值（x ||= y）运算仅在 x 为假值时为其赋值
+  err.status ||= '500'; // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Logical_OR_assignment
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
