4b21ec8de991173a6c8499092c60d89e29eb058c
lupinus <bzirs@outlook.com>
2025-09-19 20:42:53 +0800

    :new: feat: 通过express-rate-limit限制ip请求次数，添加helmet 给响应头添加安全选项

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const toursRouter = require('./routes/tours');
 const usersRouter = require('./routes/users');
@@ -8,12 +10,24 @@ const errorhandler = require('./controller/error');
 
 const app = express();
 
+// 设置express安全头部
+app.use(helmet());
+
 console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === 'development') {
   // morgan 日志中间件
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+// 限制 /api 路由下 一小时最多请求100次 一个ip 会在响应头中添加 X-RateLimit-Limit  X-RateLimit-Remaining
+app.use('/api', limiter);
 
 // TODO 中间件
 // json 读取请求体body中的值 static 读取静态文件目录中间件
